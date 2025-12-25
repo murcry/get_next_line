3c0b42229fbdabe86cd491161357d6ac5876102e
murcry <diego.brasero@gmail.com>
2025-12-25 23:21:58 +0100

    FINAL

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: digonza2 <digonza2@student.42madrid.com>   +#+  +:+       +#+        */
+/*   By: digonza2 <digonza2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 17:12:10 by digonza2          #+#    #+#             */
-/*   Updated: 2025/12/23 19:04:08 by digonza2         ###   ########.fr       */
+/*   Updated: 2025/12/25 23:12:06 by digonza2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 int	main(void)
 {
-	char	*line1;
-	char	*line2;
+	char	*line;
 	int fd1 = open("pruebas/empty.txt", O_RDONLY);
 	int fd2 = open("pruebas/AlbionOnline.txt", O_RDONLY);
-	line1 = get_next_line(fd1);
-	line2 = get_next_line(fd2);
-	while(line1 || line2)
+	line = get_next_line(fd1);
+	while (line)
 	{
-		printf("%s", line1);
-		free(line1);
-		line2 = get_next_line(fd2);
-		printf("%s", line2);
-		free(line2);
-		line1 = get_next_line(fd1);
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd1);
 	}
-	free(line1);
-	free(line2);
 	close(fd1);
+	line = get_next_line(fd2);
+	while (line)
+	{
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd2);
+	}
 	close(fd2);
 	return (0);
 }
